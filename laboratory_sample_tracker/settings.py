fa4fe034950447c2eb38eff3da9bc4324c0f2bec
Tyler Varzeas <tyler.varzeas@gmail.com>
2025-10-22 19:29:22 -0700

    Set up for render deplou

--- laboratory_sample_tracker/settings.py
+++ laboratory_sample_tracker/settings.py
@@ -10,23 +10,29 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+import dj_database_url
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+if os.path.isfile(os.path.join(BASE_DIR, '.env')):
+    load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d!164*tjhmx@7i$jzu(rsb-l^lyfz13trbuic+w_8ep(9!x#m$'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'a-default-weak-key-for-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
-
+allowed_hosts_str = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost')
+ALLOWED_HOSTS = allowed_hosts_str.split(',')
 
 # Application definition
 
@@ -79,14 +85,10 @@ WSGI_APPLICATION = 'laboratory_sample_tracker.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        "ENGINE": "django.db.backends.postgresql",
-        "USER": "lims_user",
-        "NAME": "lims_db",
-        "PASSWORD": "password",
-        "HOST": "127.0.0.1",
-        "PORT": "5432"
-    }
+    'default': dj_database_url.parse(
+        str(os.environ.get('DATABASE_URL')),
+        conn_max_age=600 
+    )
 }
 
 
